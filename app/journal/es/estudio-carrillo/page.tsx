8d50da45566142629a2156af7b10492c140a3b20
javikin <javikin10@gmail.com>
2025-11-28 02:20:46 -0500

    Remove old ES journal pages

--- app/journal/es/estudio-carrillo/page.tsx
+++
@@ -1,6 +0,0 @@
-import BlogPost from '@/components/journal/BlogPost';
-import { content } from '../../carrillo-studio/content.es';
-
-export default function EstudioCarrilloPost() {
-  return <BlogPost content={content} backLabel="Volver al Journal" />;
-}
